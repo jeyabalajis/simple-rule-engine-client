58c7b70a5674aae84ec8c911fd26e630dc92c9be
Jeya <jeyabalaji.subramanian@gmail.com>
2022-09-29 11:15:52 +0530

    implemented rule score and operators

--- services/adapter/simple_rule_engine_adapter.py
+++ services/adapter/simple_rule_engine_adapter.py
@@ -1,16 +1,28 @@
-from simpleruleengine.expression.expression import Expression
-from simpleruleengine.operator.between import Between
-from simpleruleengine.rule.rule import Rule
-from simpleruleengine.rule.rule_decision import RuleDecision
-from simpleruleengine.rulerow.rule_row_decision import RuleRowDecision
+from typing import Union
+
 from simpleruleengine.conditional.conditional import Conditional
 from simpleruleengine.conditional.when_all import WhenAll
 from simpleruleengine.conditional.when_any import WhenAny
-from simpleruleengine.ruleset.rule_set_decision import RuleSetDecision
-from simpleruleengine.token.token import Token
-from simpleruleengine.token.string_token import StringToken
-from simpleruleengine.token.numeric_token import NumericToken
+from simpleruleengine.expression.expression import Expression
+from simpleruleengine.operator.between import Between
+from simpleruleengine.operator.equal import Eq
+from simpleruleengine.operator.greater_than import Gt
+from simpleruleengine.operator.greater_than_equal import Gte
+from simpleruleengine.operator.less_than import Lt
+from simpleruleengine.operator.less_than_equal import Lte
+from simpleruleengine.operator.operator import Operator
 from simpleruleengine.operator.string_in import In
+from simpleruleengine.operator.string_not_in import NotIn
+from simpleruleengine.rule.rule import Rule
+from simpleruleengine.rule.rule_decision import RuleDecision
+from simpleruleengine.rule.rule_score import RuleScore
+from simpleruleengine.rulerow.rule_row_decision import RuleRowDecision
+from simpleruleengine.rulerow.rule_row_score import RuleRowScore
+from simpleruleengine.ruleset.rule_set_decision import RuleSetDecision
+from simpleruleengine.ruleset.rule_set_score import RuleSetScore
+from simpleruleengine.token.numeric_token import NumericToken
+from simpleruleengine.token.string_token import StringToken
+from simpleruleengine.token.token import Token
 
 
 class SimpleRuleEngineAdapter:
@@ -40,6 +52,36 @@ class SimpleRuleEngineAdapter:
                 *tuple(rule_set_objects)
             )
 
+        # Parse as Score
+        if "RuleScore" in self.rule_dict:
+            rule_sets = self.rule_dict.get("RuleScore").get("RuleSets")
+            rule_set_objects = []
+            for rule_set in rule_sets:
+                rule_row_objects = []
+                rule_rows = rule_set.get("RuleRows")
+                for rule_row in rule_rows:
+                    rule_row_objects.append(
+                        _get_rule_row_score(rule_row)
+                    )
+
+                rule_set_objects.append(
+                    RuleSetScore(
+                        *tuple(rule_row_objects),
+                        weight=rule_set.get("Weight")
+                    )
+                )
+
+            return RuleScore(
+                *tuple(rule_set_objects)
+            )
+
+
+def _get_rule_row_score(rule_row: dict) -> RuleRowScore:
+    return RuleRowScore(
+        antecedent=_get_conditional(rule_row),
+        consequent=rule_row.get("Consequent")
+    )
+
 
 def _get_rule_row_decision(rule_row: dict) -> RuleRowDecision:
     return RuleRowDecision(
@@ -48,7 +90,7 @@ def _get_rule_row_decision(rule_row: dict) -> RuleRowDecision:
     )
 
 
-def _get_conditional(rule_row: dict):
+def _get_conditional(rule_row: dict) -> Union[Conditional, Expression]:
     if "WhenAll" in rule_row:
         expressions = []
         for token in rule_row.get("WhenAll"):
@@ -58,24 +100,60 @@ def _get_conditional(rule_row: dict):
 
         return WhenAll(*tuple(expressions))
 
-    token = None
-    operator = None
-    if "StringToken" in rule_row:
-        token = StringToken(name=rule_row.get("StringToken"))
-
-        if "In" in rule_row:
-            operator = In(*tuple(rule_row.get("In")))
-
-    if "NumericToken" in rule_row:
-        token = NumericToken(name=rule_row.get("NumericToken"))
-
-        if "Between" in rule_row:
-            operator = Between(
-                floor=rule_row.get("Between").get("floor"),
-                ceiling=rule_row.get("Between").get("ceiling")
+    if "WhenAny" in rule_row:
+        expressions = []
+        for token in rule_row.get("WhenAny"):
+            expressions.append(
+                _get_conditional(token)
             )
+
+        return WhenAny(*tuple(expressions))
+
+    return _get_expression(rule_row)
+
+
+def _get_expression(expression_dict: dict) -> Expression:
+    token = _get_token(expression_dict)
+    operator = _get_operator(expression_dict)
 
     assert token is not None
     assert operator is not None
 
     return Expression(token=token, operator=operator)
+
+
+def _get_token(expression_dict: dict) -> Token:
+    if "StringToken" in expression_dict:
+        return StringToken(name=expression_dict.get("StringToken"))
+
+    if "NumericToken" in expression_dict:
+        return NumericToken(name=expression_dict.get("NumericToken"))
+
+
+def _get_operator(expression_dict: dict) -> Operator:
+    if "In" in expression_dict:
+        return In(*tuple(expression_dict.get("In")))
+
+    if "NotIn" in expression_dict:
+        return NotIn(*tuple(expression_dict.get("NotIn")))
+
+    if "Between" in expression_dict:
+        return Between(
+            floor=expression_dict.get("Between").get("floor"),
+            ceiling=expression_dict.get("Between").get("ceiling")
+        )
+
+    if "Eq" in expression_dict:
+        return Eq(expression_dict.get("Eq"))
+
+    if "Lt" in expression_dict:
+        return Lt(expression_dict.get("Lt"))
+
+    if "Lte" in expression_dict:
+        return Lte(expression_dict.get("Lte"))
+
+    if "Gt" in expression_dict:
+        return Gt(expression_dict.get("Gt"))
+
+    if "Gte" in expression_dict:
+        return Gte(expression_dict.get("Gte"))
--- test_simple_rule_engine_adapter.py
+++ test_simple_rule_engine_adapter.py
@@ -7,9 +7,9 @@ from services.util.json_file_util import JsonFileUtil
 class TestSimpleRuleEngineAdapter(TestCase):
     def test_rule_simple_decision(self):
         json_file_util = JsonFileUtil(file_name_with_path="./examples/simple_decision.json")
-        decision_dict = json_file_util.read_file()
+        decision_rule_dict = json_file_util.read_file()
 
-        rule_engine_adapter = SimpleRuleEngineAdapter(rule_dict=decision_dict)
+        rule_engine_adapter = SimpleRuleEngineAdapter(rule_dict=decision_rule_dict)
         decision_rule = rule_engine_adapter.get_rule()
 
         assert type(decision_rule).__name__ == "RuleDecision"
@@ -17,3 +17,32 @@ class TestSimpleRuleEngineAdapter(TestCase):
         fact = dict(cibil_score=700, business_ownership="Owned by Self")
         assert decision_rule.execute(token_dict=fact) == "GO"
 
+    def test_rule_simple_decision_when_any(self):
+        json_file_util = JsonFileUtil(file_name_with_path="./examples/simple_decision_when_any.json")
+        decision_rule_dict = json_file_util.read_file()
+
+        rule_engine_adapter = SimpleRuleEngineAdapter(rule_dict=decision_rule_dict)
+        decision_rule = rule_engine_adapter.get_rule()
+
+        assert type(decision_rule).__name__ == "RuleDecision"
+
+        fact = dict(age=35, pet="parrot")
+        assert decision_rule.execute(token_dict=fact) == "GO"
+
+        fact = dict(age=20, pet="dog")
+        assert decision_rule.execute(token_dict=fact) != "GO"
+
+    def test_rule_simple_score(self):
+        json_file_util = JsonFileUtil(file_name_with_path="./examples/simple_score.json")
+        score_rule_dict = json_file_util.read_file()
+
+        rule_engine_adapter = SimpleRuleEngineAdapter(rule_dict=score_rule_dict)
+        score_rule = rule_engine_adapter.get_rule()
+
+        assert type(score_rule).__name__ == "RuleScore"
+
+        fact = dict(age=40, pet="dog", domicile="TN")
+        assert score_rule.execute(token_dict=fact) == 5.0
+
+        fact = dict(age=40, pet="dog", domicile="KA")
+        assert score_rule.execute(token_dict=fact) == 7.5
