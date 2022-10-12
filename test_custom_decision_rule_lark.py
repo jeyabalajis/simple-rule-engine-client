efdee500924d595e8707f57485e38170ae64e25d
JeyabalajiSubramanian <jeyabalaji_subramanian@cargill.com>
2022-10-12 14:56:50 +0530

    Added Pytest fixture for lark grammar

--- test_custom_decision_rule_lark.py
+++ test_custom_decision_rule_lark.py
@@ -1,84 +1,139 @@
 from unittest import TestCase
-
+import pytest
 from lark import Lark
 
 from services.transformer.simple_rule_engine_transformer import SimpleRuleEngineTransformer
 
+@pytest.fixture
+def decision_rule_grammar():
+    with open("./decision_rule.lark") as rule_grammar_file:
+        rule_grammar = rule_grammar_file.read()
+    
+    return rule_grammar
 
-class TestCustomDecisionRuleLark(TestCase):
-    def test_rule_complex_decision(self):
-        with open("./decision_rule.lark") as rule_grammar_file:
-            rule_grammar = rule_grammar_file.read()
+def test_rule_simple_decision(decision_rule_grammar):
 
-        parser = Lark(rule_grammar)
+    parser = Lark(decision_rule_grammar)
 
-        custom_rule = """
-            my_rule {
-                when {
-                    cibil_score between 650 and 750 and 
-                    age > 35 and 
-                    house_ownership in (owned, rented) and
-                    (
-                        total_overdue_amount == 0 or 
-                        number_of_overdue_loans < 2 or
-                        (
-                            number_of_overdue_loans >= 2 and
-                            big_shot == true
-                        )
-                    ) and
-                    pet == dog
-                }
-                then true
-                when {
-                    cibil_score < 650
-                }
-                then false
+    custom_rule = """
+        my_rule {
+            when {
+                cibil_score > 650 and
+                age > 35 and
+                house_ownership in (owned, rented) and
+                (pet == dog or pet == parrot)
             }
-            """
+            then true
+            when {
+                cibil_score < 650
+            }
+            then false
+        }
+        """
 
-        tree = parser.parse(custom_rule)
-        print(tree.pretty())
+    tree = parser.parse(custom_rule)
+    print(tree.pretty())
 
-        decision_rule = SimpleRuleEngineTransformer(tree).get_rule()
+    decision_rule = SimpleRuleEngineTransformer(tree).get_rule()
 
-        # Evaluate the Decision Rule by passing data
-        facts = dict(
-            cibil_score=700,
-            age=40,
-            house_ownership="owned",
-            total_overdue_amount=0,
-            pet="dog"
-        )
-        assert decision_rule.execute(token_dict=facts) is True
+    facts = dict(
+        cibil_score=700,
+        age=40,
+        house_ownership="owned",
+        pet="parrot"
+    )
 
-        facts = dict(
-            cibil_score=700,
-            age=40,
-            house_ownership="owned",
-            total_overdue_amount=100,
-            number_of_overdue_loans=1,
-            pet="dog"
-        )
-        assert decision_rule.execute(token_dict=facts) is True
+    assert decision_rule.execute(facts) is True
 
-        facts = dict(
-            cibil_score=700,
-            age=40,
-            house_ownership="owned",
-            total_overdue_amount=100,
-            number_of_overdue_loans=2,
-            big_shot="true",
-            pet="dog"
-        )
-        assert decision_rule.execute(token_dict=facts) is True
+    facts = dict(
+        cibil_score=700,
+        age=40,
+        house_ownership="owned",
+        pet="pig"
+    )
 
-        facts = dict(
-            cibil_score=600,
-            age=40,
-            house_ownership="owned",
-            total_overdue_amount=100,
-            number_of_overdue_loans=2,
-            big_shot="false",
-            pet="dog"
-        )
-        assert decision_rule.execute(token_dict=facts) is False
+    assert decision_rule.execute(facts) is not True    
+
+    facts = dict(
+        cibil_score=500,
+        age=40,
+        house_ownership="owned",
+        pet="dog"
+    )
+
+    assert decision_rule.execute(facts) is not True  
+
+def test_rule_complex_decision(decision_rule_grammar):
+
+    parser = Lark(decision_rule_grammar)
+
+    custom_rule = """
+        my_rule {
+            when {
+                cibil_score between 650 and 750 and 
+                age > 35 and 
+                house_ownership in (owned, rented) and
+                (
+                    total_overdue_amount == 0 or 
+                    number_of_overdue_loans < 2 or
+                    (
+                        number_of_overdue_loans >= 2 and
+                        big_shot == true
+                    )
+                ) and
+                pet == dog
+            }
+            then true
+            when {
+                cibil_score < 650
+            }
+            then false
+        }
+        """
+
+    tree = parser.parse(custom_rule)
+    print(tree.pretty())
+
+    decision_rule = SimpleRuleEngineTransformer(tree).get_rule()
+
+    # Evaluate the Decision Rule by passing data
+    facts = dict(
+        cibil_score=700,
+        age=40,
+        house_ownership="owned",
+        total_overdue_amount=0,
+        pet="dog"
+    )
+    assert decision_rule.execute(token_dict=facts) is True
+
+    facts = dict(
+        cibil_score=700,
+        age=40,
+        house_ownership="owned",
+        total_overdue_amount=100,
+        number_of_overdue_loans=1,
+        pet="dog"
+    )
+    assert decision_rule.execute(token_dict=facts) is True
+
+    facts = dict(
+        cibil_score=700,
+        age=40,
+        house_ownership="owned",
+        total_overdue_amount=100,
+        number_of_overdue_loans=2,
+        big_shot="true",
+        pet="dog"
+    )
+    assert decision_rule.execute(token_dict=facts) is True
+
+    facts = dict(
+        cibil_score=600,
+        age=40,
+        house_ownership="owned",
+        total_overdue_amount=100,
+        number_of_overdue_loans=2,
+        big_shot="false",
+        pet="dog"
+    )
+    assert decision_rule.execute(token_dict=facts) is False
